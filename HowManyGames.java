8d22cd816b4e54dba1133008316f06aef233a2f5
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-06-09 22:25:30 -0400

    initial upload

---
+++ HowManyGames.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HowManyGames {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many people are playing? ");
+
+        while (!input.hasNextInt()) {
+            System.out.println("Invalid input. Please enter an integer.");
+            input.next();
+        }
+
+        int numberOfPeople = input.nextInt();
+        int numberOfGames = numberOfPeople / 4;
+        System.out.print("There is enough people for " + numberOfGames + " game(s)");
+        input.close();
+    }
+}
