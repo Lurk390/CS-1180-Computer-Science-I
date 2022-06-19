24387444f1a123576d81ca27e062d26c581deb7e
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-06-19 01:54:03 -0400

    Reformated question and comments

--- P2_CreditCardValidation.java
+++ P2_CreditCardValidation.java
@@ -51,34 +51,28 @@ public class P2_CreditCardValidation {
         return creditCardType;
     }
 
-    // calculates the sum of every other digit from right to left starting at the
-    // rightmost digit
+    // Calculates the sum of every other digit from right to left starting at the rightmost digit
     public static int oddSum(long creditCardInput) {
         int sumOdd = 0;
 
         while (creditCardInput > 0) {
-            sumOdd += creditCardInput % 10; // returns the remainder when the card number is divided by 10 (last digit)
-                                            // and adds it to the overall sum
-            creditCardInput /= 100; // skips two digits from right to left
+            sumOdd += creditCardInput % 10; // Returns the remainder when the card number is divided by 10 (last digit) and adds it to the overall sum
+            creditCardInput /= 100; // Skips two digits from right to left
         }
         return sumOdd;
     }
 
-    // calculates sum of every other digit right to left starting from second
-    // rightmost digit
+    // Calculates sum of every other digit right to left starting from second rightmost digit
     public static int evenSum(long creditCardInput) {
         int sumEven = 0;
         while (creditCardInput > 0) {
-            double evenSingleDigit = (2 * Math.floor((creditCardInput % 100) / 10)); // returns the remainder when the
-                                                                                     // card number is divided by 100
-                                                                                     // (last two digits), then divide
-                                                                                     // by 10 then floor it for the
-                                                                                     // first digit (ex. 26 = 2)
-            if (evenSingleDigit > 9) { // if evenSingleDigit is two digits, add each digit together
+            double evenSingleDigit = (2 * Math.floor((creditCardInput % 100) / 10)); // Returns the remainder when the card number is divided by 100(last two digits), 
+                                                                                     // then divide by 10 then floor it for the first digit (ex. 26 = 2)
+            if (evenSingleDigit > 9) { // If evenSingleDigit is two digits, add each digit together
                 evenSingleDigit = (evenSingleDigit - 10) + 1;
             }
-            sumEven += evenSingleDigit; // add evenSingleDigit to the overall sum
-            creditCardInput /= 100; // skips two digits from right to left
+            sumEven += evenSingleDigit; // Add evenSingleDigit to the overall sum
+            creditCardInput /= 100; // Skips two digits from right to left
         }
         return sumEven;
     }
@@ -96,62 +90,55 @@ public class P2_CreditCardValidation {
 }
 
 /*
- * Credit card numbers follow certain patterns. A credit card number must have
- * between
- * 13 and 16 digits. It must start with:
- * 
- * 4 for Visa cards
- * 5 for Master cards
- * 37 for American Express cards
- * 6 for Discover cards
- * 
- * In 1954, Hans Luhn of IBM proposed an algorithm for validating credit card
- * numbers.
- * The algorithm is useful to determine if a card number is entered correctly or
- * if a credit
- * card is scanned correctly by a scanner. Almost all credit card numbers are
- * generated
- * following this validity check, commonly known as the Luhn check or the Mod 10
- * check,
- * which can be described as follows (for illustration, consider the card number
- * 4388576018402626):
- * 
- * 1. Double every second digit from right to left. If doubling of a digit
- * results in a two-digit
- * number, add up the two digits to get a single-digit number.
- * 2 * 2 = 4
- * 2 * 2 = 4
- * 4 * 2 = 8
- * 1 * 2 = 2
- * 6 * 2 = 12 (1 + 2 = 3)
- * 5 * 2 = 10 (1 + 0 = 1)
- * 8 * 2 = 16 (1 + 6 = 7)
- * 4 * 2 = 8
- * 
- * 2. Now add all single-digit numbers from Step 1.
- * 4 + 4 + 8 + 2 + 3 + 1 + 7 + 8 = 37
- * 
- * 3. Add all digits in the odd places from right to left in the card number.
- * 6 + 6 + 0 + 8 + 0 + 7 + 8 + 3 = 38
- * 
- * 4. Sum the results from Step 2 and Step 3.
- * 37 + 38 = 75
- * 
- * 5. If the result from Step 4 is divisible by 10, the card number is valid;
- * otherwise, it is
- * invalid. For example, the number 4388576018402626 is invalid, but the number
- * 4388576018410707 is valid.
- * 
- * Write a program that prompts the user to enter a credit card number as a long
- * integer.
- * Display whether the number is valid or invalid.
- * 
- * Here are sample runs of the program:
- * Sample 1:
- * Enter a credit card number as a long integer: 4246345689049834
- * 4246345689049834 is invalid
- * 
- * Sample 2:
- * Enter a credit card number as a long integer: 4388576018410707
- * 4388576018410707 is valid
- */
+Credit card numbers follow certain patterns. A credit card number must have between
+13 and 16 digits. It must start with:
+
+4 for Visa cards
+5 for Master cards
+37 for American Express cards
+6 for Discover cards
+
+In 1954, Hans Luhn of IBM proposed an algorithm for validating credit card numbers.
+The algorithm is useful to determine if a card number is entered correctly or if a credit
+card is scanned correctly by a scanner. Almost all credit card numbers are generated
+following this validity check, commonly known as the Luhn check or the Mod 10 check,
+which can be described as follows (for illustration, consider the card number
+4388576018402626):
+
+1. Double every second digit from right to left. If doubling of a digit results in a 
+two-digit number, add up the two digits to get a single-digit number.
+2 * 2 = 4
+2 * 2 = 4
+4 * 2 = 8
+1 * 2 = 2
+6 * 2 = 12 (1 + 2 = 3)
+5 * 2 = 10 (1 + 0 = 1)
+8 * 2 = 16 (1 + 6 = 7)
+4 * 2 = 8
+
+2. Now add all single-digit numbers from Step 1.
+4 + 4 + 8 + 2 + 3 + 1 + 7 + 8 = 37
+
+3. Add all digits in the odd places from right to left in the card number.
+6 + 6 + 0 + 8 + 0 + 7 + 8 + 3 = 38
+
+4. Sum the results from Step 2 and Step 3.
+37 + 38 = 75
+
+5. If the result from Step 4 is divisible by 10, the card number is valid; otherwise, it is
+invalid. For example, the number 4388576018402626 is invalid, but the number 
+4388576018410707 is valid.
+
+Write a program that prompts the user to enter a credit card number as a long integer.
+Display whether the number is valid or invalid.
+
+Here are sample runs of the program:
+
+Sample 1:
+Enter a credit card number as a long integer: 4246345689049834
+4246345689049834 is invalid
+
+Sample 2:
+Enter a credit card number as a long integer: 4388576018410707
+4388576018410707 is valid
+*/
