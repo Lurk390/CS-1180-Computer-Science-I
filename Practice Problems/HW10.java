515f278aeb2498f2587aeb9a4b4508ddc22bd592
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-07-15 22:12:07 -0400

    Added directions

--- Practice Problems/HW10.java
+++ Practice Problems/HW10.java
@@ -2,3 +2,30 @@ public class HW10 {
     public static void main(String[] args) {
     }
 }
+
+/*
+Design but do not implement a Tic-Tac-Toe program that meets the requirements listed below. Your
+design should include a list of classes. For each class, you should list its fields (access modifier, data type,
+and name) and methods (access modifier, return type, name, and parameter list). You should also
+provide extensive comments for each field and method. For fields, state their intended purpose and
+valid values (for example, “this field stores the user’s score; it must be greater than or equal to zero”)
+and for methods, state what each parameter should contain, any preconditions for the method to
+operate correctly, what the method should do, what it will return, and any postconditions (i.e. things
+that are true after the method completes). Note that not all methods will have all of these things (i.e. it
+is possible that a method does not have any preconditions). Remember that a good Java method is like a
+good English paragraph — it should do/say one thing well. Also remember to enforce the object-
+oriented principles of encapsulation and reusing code wherever possible rather than duplicating it. You
+may turn in either a PDF with your design or Java code stubs and comments.
+
+TicTacToe Requirements
+
+Support for one human and one computer player or two human players
+Keep track of players’ win-loss-tie records between program executions (e.g. by persisting data in a file).
+Display a list of the top ten best records when the game starts up.
+Randomly select one of the players to choose their symbol (X or O)
+Standard TicTacToe rules apply (X moves first, players alternate turns, you can’t move to a square that is
+already occupied, three in a row horizontally, vertically or diagonally wins)
+When the game ends, the result is announced, players’ records are updated, and the user(s) are asked if
+they want to play again 
+
+ */
